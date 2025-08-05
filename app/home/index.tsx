e0afaa104e029683859acfe268e63fd1297dfbbf
hosoya08 <118666052+hosoya08@users.noreply.github.com>
2025-08-05 22:03:33 +0900

    [update] ホーム画面のコーディング

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,1 +1,23 @@
 // ホーム画面
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>ホーム画面</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#EFEFF4",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold"
+    }
+})
