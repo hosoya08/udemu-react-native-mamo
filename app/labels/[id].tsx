0636b7a44ee49af82e14e9f8132edf8bb64cde13
hosoya08 <118666052+hosoya08@users.noreply.github.com>
2025-08-06 09:40:46 +0900

    [update]ホームからラベル追加画面、ラベル1/2修正画面の遷移実装

--- app/labels/[id].tsx
+++ app/labels/[id].tsx
@@ -1,1 +1,32 @@
 // ラベル編集画面
+import { router, useLocalSearchParams } from 'expo-router';
+import { useEffect } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function LabelEditScreen() {
+
+    const { id } = useLocalSearchParams();
+
+    const handleEditPress = ()=> {
+        router.dismiss();
+    }
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>ラベル修正： {id}</Text>
+            <Button title='修正' onPress={handleEditPress} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#EFEFF4",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold"
+    }
+})
