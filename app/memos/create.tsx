93e5b1d07207e5963c0402803e56fce9416ad9a5
hosoya08 <118666052+hosoya08@users.noreply.github.com>
2025-08-14 16:50:58 +0900

    [update] メモ一覧画面、メモ作成画面、メモ修正画面の作成

--- app/memos/create.tsx
+++ app/memos/create.tsx
@@ -1,1 +1,29 @@
 // メモ作成画面
+import { router } from 'expo-router';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function MemoCreateScreen() {
+    const handleCreatePress = () => {
+        router.back(); // backは一画面前に戻る
+    };
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>メモ作成</Text>
+            <Button title="作成" onPress={handleCreatePress} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#EFEFF4',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+});
--- app/memos/index.tsx
+++ app/memos/index.tsx
@@ -1,1 +1,37 @@
 // メモ一覧画面
+import { router, useLocalSearchParams, usePathname } from 'expo-router';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function MemoListScreen() {
+    const { labelId } = useLocalSearchParams();
+
+    const handleCreatePress = () => {
+        router.push({ pathname: '/memos/create' });
+    };
+    const handleMemoPress = (memoId: string) => {
+        router.push({ pathname: `/memos/${memoId}` });
+    };
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>{labelId ? `ラベルID： ${labelId}` : 'すべてのメモ'}</Text>
+            <Button title="メモ作成" onPress={handleCreatePress} />
+
+            <Button title="メモ1" onPress={() => handleMemoPress('ABC')} />
+            <Button title="メモ2" onPress={() => handleMemoPress('DEF')} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#EFEFF4',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+});
